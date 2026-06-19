5d14a4d623776ecb5e0c88e9390714fad5eaae75
MiniMaryo <miniminizero15@gmail.com>
2026-06-19 03:08:34 +0300

    Match add form layout with assignment

--- LabWork6_Kunev_Anton/FormAddUPDMD.Designer.cs
+++ LabWork6_Kunev_Anton/FormAddUPDMD.Designer.cs
@@ -49,9 +49,9 @@
             this.pictureBox1.BackColor = System.Drawing.SystemColors.ControlDark;
             this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.pictureBox1.Location = new System.Drawing.Point(541, 87);
+            this.pictureBox1.Location = new System.Drawing.Point(394, 83);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(213, 117);
+            this.pictureBox1.Size = new System.Drawing.Size(167, 86);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
@@ -59,10 +59,9 @@
             // 
             // buttonBack
             // 
-            this.buttonBack.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
-            this.buttonBack.Location = new System.Drawing.Point(30, 28);
+            this.buttonBack.Location = new System.Drawing.Point(89, 41);
             this.buttonBack.Name = "buttonBack";
-            this.buttonBack.Size = new System.Drawing.Size(99, 38);
+            this.buttonBack.Size = new System.Drawing.Size(46, 23);
             this.buttonBack.TabIndex = 1;
             this.buttonBack.Text = "Назад";
             this.buttonBack.UseVisualStyleBackColor = true;
@@ -70,10 +69,9 @@
             // 
             // buttonAdd
             // 
-            this.buttonAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
-            this.buttonAdd.Location = new System.Drawing.Point(580, 280);
+            this.buttonAdd.Location = new System.Drawing.Point(430, 226);
             this.buttonAdd.Name = "buttonAdd";
-            this.buttonAdd.Size = new System.Drawing.Size(131, 48);
+            this.buttonAdd.Size = new System.Drawing.Size(102, 23);
             this.buttonAdd.TabIndex = 2;
             this.buttonAdd.Text = "Добавить";
             this.buttonAdd.UseVisualStyleBackColor = true;
@@ -82,97 +80,97 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label1.Location = new System.Drawing.Point(37, 130);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(95, 88);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(92, 26);
+            this.label1.Size = new System.Drawing.Size(51, 13);
             this.label1.TabIndex = 3;
             this.label1.Text = "Модель";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label2.Location = new System.Drawing.Point(37, 180);
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(95, 124);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(77, 26);
+            this.label2.Size = new System.Drawing.Size(45, 13);
             this.label2.TabIndex = 4;
             this.label2.Text = "Марка";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label3.Location = new System.Drawing.Point(37, 230);
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(95, 160);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(64, 26);
+            this.label3.Size = new System.Drawing.Size(37, 13);
             this.label3.TabIndex = 5;
             this.label3.Text = "Цена";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label4.Location = new System.Drawing.Point(37, 280);
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(95, 196);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(50, 26);
+            this.label4.Size = new System.Drawing.Size(30, 13);
             this.label4.TabIndex = 6;
             this.label4.Text = "Л/С";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label5.Location = new System.Drawing.Point(37, 325);
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(95, 232);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(85, 26);
+            this.label5.Size = new System.Drawing.Size(50, 13);
             this.label5.TabIndex = 7;
             this.label5.Text = "Пробег";
             // 
             // textBoxModel
             // 
-            this.textBoxModel.Location = new System.Drawing.Point(181, 130);
+            this.textBoxModel.Location = new System.Drawing.Point(163, 85);
             this.textBoxModel.Name = "textBoxModel";
-            this.textBoxModel.Size = new System.Drawing.Size(306, 20);
+            this.textBoxModel.Size = new System.Drawing.Size(214, 20);
             this.textBoxModel.TabIndex = 9;
             // 
             // textBoxPrice
             // 
-            this.textBoxPrice.Location = new System.Drawing.Point(181, 230);
+            this.textBoxPrice.Location = new System.Drawing.Point(163, 157);
             this.textBoxPrice.Name = "textBoxPrice";
-            this.textBoxPrice.Size = new System.Drawing.Size(306, 20);
+            this.textBoxPrice.Size = new System.Drawing.Size(214, 20);
             this.textBoxPrice.TabIndex = 10;
             this.textBoxPrice.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBoxPrice_KeyPress);
             // 
             // textBoxMileage
             // 
-            this.textBoxMileage.Location = new System.Drawing.Point(181, 325);
+            this.textBoxMileage.Location = new System.Drawing.Point(163, 229);
             this.textBoxMileage.Name = "textBoxMileage";
-            this.textBoxMileage.Size = new System.Drawing.Size(306, 20);
+            this.textBoxMileage.Size = new System.Drawing.Size(214, 20);
             this.textBoxMileage.TabIndex = 11;
             this.textBoxMileage.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBoxMileage_KeyPress);
             // 
             // textBoxHorsepower
             // 
-            this.textBoxHorsepower.Location = new System.Drawing.Point(181, 280);
+            this.textBoxHorsepower.Location = new System.Drawing.Point(163, 193);
             this.textBoxHorsepower.Name = "textBoxHorsepower";
-            this.textBoxHorsepower.Size = new System.Drawing.Size(306, 20);
+            this.textBoxHorsepower.Size = new System.Drawing.Size(214, 20);
             this.textBoxHorsepower.TabIndex = 12;
             this.textBoxHorsepower.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBoxHorsepower_KeyPress);
             // 
             // comboBoxBrand
             // 
             this.comboBoxBrand.FormattingEnabled = true;
-            this.comboBoxBrand.Location = new System.Drawing.Point(181, 180);
+            this.comboBoxBrand.Location = new System.Drawing.Point(163, 121);
             this.comboBoxBrand.Name = "comboBoxBrand";
-            this.comboBoxBrand.Size = new System.Drawing.Size(306, 21);
+            this.comboBoxBrand.Size = new System.Drawing.Size(214, 21);
             this.comboBoxBrand.TabIndex = 13;
             // 
             // FormAddUPDMD
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 384);
+            this.ClientSize = new System.Drawing.Size(584, 271);
             this.Controls.Add(this.comboBoxBrand);
             this.Controls.Add(this.textBoxHorsepower);
             this.Controls.Add(this.textBoxMileage);
@@ -187,7 +185,7 @@
             this.Controls.Add(this.buttonBack);
             this.Controls.Add(this.pictureBox1);
             this.Name = "FormAddUPDMD";
-            this.Text = "Form1";
+            this.Text = "Добавление нового мотоцикла";
             this.Load += new System.EventHandler(this.FormAddUPDMD_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
